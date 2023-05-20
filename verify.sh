7da96443e295676dc252fa8cc0fadf2a0088bae7
Albert Xu <albert.t.xu@gmail.com>
2023-05-20 14:47:49 -0400

    added verify script

---
+++ verify.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -xe
+
+listings="
+listing37 \
+listing38 \
+listing39 \
+"
+
+sh build.sh
+
+for asm in ${listings}; do
+   nasm ${asm}.nasm
+   ./decoder ${asm} > tempfile.nasm
+   nasm tempfile.nasm
+   diff tempfile ${asm}
+done
